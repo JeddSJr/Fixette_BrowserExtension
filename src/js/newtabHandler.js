b72ddb7d211cb28af7b2555ac5c9eb2ecc70bf76
JeddSJr <55459056+JeddSJr@users.noreply.github.com>
2024-07-17 00:33:39 -0400

    more elegant portrait v landscape handling

--- src/js/newtabHandler.js
+++ src/js/newtabHandler.js
@@ -1,13 +1,14 @@
 window.addEventListener('load', function() {
     var zoomedImgContainer = document.getElementById("zoomedImgContainer");
     var newTabContainer = document.getElementById("newTabContainer");
-    var dezoomedImgContainer = document.getElementById("dezoomedImgContainer");
+
+    let dezoomedImgContainerL = document.getElementById("dezoomedImgContainerLandscape")
+    let dezoomedImgContainerP = document.getElementById("dezoomedImgContainerPortrait")
 
     var displayedImg = document.getElementById("displayImg");
 
     function changeImageZoom() {
         var zoomedImgHidden = zoomedImgContainer.getAttribute("hidden");
-        dezoomedImgContainer.src = "../imgs/default/DP215410.jpg";
         if (zoomedImgHidden) {
             zoomedImgContainer.removeAttribute("hidden");
             newTabContainer.setAttribute("hidden","hidden");
@@ -27,10 +28,10 @@ window.addEventListener('load', function() {
         true
     );
 
-    if(dezoomedImgContainer){dezoomedImgContainer.addEventListener(
-        "click",
-        changeImageZoom
-    )}
+    if(dezoomedImgContainerL || dezoomedImgContainerP){
+        dezoomedImgContainerL.addEventListener("click",changeImageZoom)
+        dezoomedImgContainerP.addEventListener("click",changeImageZoom)
+    }
 
     if(zoomedImgContainer){
         zoomedImgContainer.addEventListener(
@@ -45,9 +46,14 @@ export function setMainImg(museumImage) {
     try {
         //Make it so that the vertical images don't push back the bar
         console.log(museumImage);
-        var displayedImg = document.getElementById("displayImg");
+        
+        var displayedImgL = document.getElementById("displayImgL");
+        var displayedImgP = document.getElementById("displayImgP");
+
         var zoomedImg = document.getElementById("zoomImg")
-        let dezoomedImgContainer = document.getElementById("dezoomedImgContainer")
+
+        let dezoomedImgContainerL = document.getElementById("dezoomedImgContainerLandscape")
+        let dezoomedImgContainerP = document.getElementById("dezoomedImgContainerPortrait")
     
         console.log(museumImage.title)
 
@@ -55,26 +61,32 @@ export function setMainImg(museumImage) {
 
         var displayTitle = museumImage.title + " - " + displayArtist + " (" + museumImage.period + ")"
 
-        displayedImg.src = museumImage.imgSrc
+        
 
-        displayedImg.title = displayTitle
-        displayedImg.alt = museumImage.title;
+        displayedImgL.src = museumImage.imgSrc
+        displayedImgP.src = museumImage.imgSrc
+        
+        let nWidth = displayedImgL.naturalWidth
+        let nHeight = displayedImgL.naturalHeight
 
+        if(nWidth < nHeight){ 
+            dezoomedImgContainerP.removeAttribute("hidden")
+            dezoomedImgContainerL.setAttribute("hidden","hidden")
+            displayedImgP.title = displayTitle
+            displayedImgP.alt = museumImage.title;
+        }
+        else{ 
+            dezoomedImgContainerL.removeAttribute("hidden")
+            dezoomedImgContainerP.setAttribute("hidden","hidden")
+            displayedImgL.title = displayTitle
+            displayedImgL.alt = museumImage.title;
+        }
+         
         zoomedImg.src = museumImage.imgSrc
         zoomedImg.title = displayTitle
         zoomedImg.alt = museumImage.title;
 
-        let nWidth = displayedImg.naturalWidth
-        let nHeight = displayedImg.naturalHeight
-        console.log(nWidth)
-        console.log(nHeight)
-        if(nWidth < nHeight){ 
-            dezoomedImgContainer.style.maxWidth = "55%";
-        }
-        else{ 
-            dezoomedImgContainer.style.maxWidth = "80%"; 
-         }
-
+        
     } catch (error) {
         console.log(error);
     }
