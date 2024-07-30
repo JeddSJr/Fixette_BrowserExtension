c8c23f82629bf72b2621bc01b960a961f31dbef3
JeddSJr <55459056+JeddSJr@users.noreply.github.com>
2024-07-29 23:37:20 -0400

    mpre popup options work with stored options on popup load

--- src/js/popupHandler.js
+++ src/js/popupHandler.js
@@ -1,18 +1,28 @@
 import {retrieveImages} from './retrieveImages.js'
 
-const numberDailyImgs = [1,2,3,4,6,8,12,24]
+const numberDailyImgs = [1,2,3,4,6,8,12]
 const searchOptionsIds = ["NumImgsOptions","EnableAIOptions"]
+const searchOptionsInputs = ["musOptions","numDailyImgsRange","enableAISelect"]
 
 window.addEventListener('load', async function() {
     var callButton = document.getElementById("callButton")
     var numDailyImgsRange = document.getElementById("numDailyImgsRange")
     var numDailyImgsRangeText = document.getElementById("numDailyImgsRangeText")
     var searchOptionsSelect = document.getElementById("searchOptionsSelect")
+    var searchOptButton = document.getElementById("searchOptButton")
+    var enableAISelect = document.getElementById("enableAISelect")
 
+    var storedOptions = await chrome.storage.sync.get("options")
+    storedOptions = storedOptions["options"]
 
     callButton.addEventListener(
         "click",
         manuallyLaunchImagesRetrieval
+    )
+
+    searchOptButton.addEventListener(
+        "click",
+        validateSearchOptions
     )
 
     numDailyImgsRange.addEventListener(
@@ -28,13 +38,17 @@ window.addEventListener('load', async function() {
         displayChosenSearchOption
     )
 
-    console.log(searchOptionsSelect)
+    if(storedOptions !== undefined){
+        numDailyImgsRange.value = numberDailyImgs.indexOf(storedOptions["numDailyImgsRange"])
+        numDailyImgsRangeText.innerText = "Number of images per day : "+storedOptions["numDailyImgsRange"]
+        enableAISelect.checked = storedOptions["enableAISelect"]
+    }
 
     var isLoadingImgs = await chrome.storage.sync.get("isLoadingImgs")
     isLoadingImgs = isLoadingImgs["isLoadingImgs"]
     console.log("isLoadingImgs: "+isLoadingImgs)
     buttonLoadingState(isLoadingImgs)
-    
+
 })
 
 chrome.storage.onChanged.addListener(async function(changes, namespace) {
@@ -47,24 +61,39 @@ chrome.storage.onChanged.addListener(async function(changes, namespace) {
 
 export function manuallyLaunchImagesRetrieval(){
     console.log("Manually launching images retrieval")
-    var inputs = document.getElementById("musOptions").getElementsByTagName("input")
-    var museumToUse = inputs[0].checked ? "Met" : "Louvre"
-    var ppOpt ={
-        museum : museumToUse,
-        //medium:null
-    }
-
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {type:"getInLoadingState"}, (response)=>{
         });
     })
     buttonLoadingState(true)
+    var ppOpt = validateSearchOptions()
     retrieveImages(ppOpt)
     storeOptions(ppOpt)
 }
 
+function validateSearchOptions(){
+    var ppOpt ={ }
+
+    searchOptionsInputs.forEach((inputId,i)=>{
+        switch(inputId){
+            case "musOptions":
+                ppOpt["museum"] = document.querySelector("input[name='"+ inputId+"']:checked").value
+                break
+            case "numDailyImgsRange":
+                ppOpt[inputId] = numberDailyImgs[document.getElementById(inputId).value]
+                break
+            case "enableAISelect":
+                ppOpt[inputId] = document.getElementById(inputId).checked
+                break
+        }
+    })
+
+    console.log(ppOpt)
+    storeOptions(ppOpt)
+    return ppOpt
+}
+
 function displayChosenSearchOption(event){
-    console.log(event.target.value)
     searchOptionsIds.forEach((optId,id)=>{
         if(id == event.target.value){
             document.getElementById(optId).removeAttribute("hidden")
@@ -73,8 +102,9 @@ function displayChosenSearchOption(event){
             document.getElementById(optId).setAttribute("hidden","hidden")
         }
     })
-        
 }
+
+
 
 export function buttonLoadingState(isLoading=false){
     var buttonSpinner = document.getElementById("buttonSpinner")
@@ -95,10 +125,10 @@ export function buttonLoadingState(isLoading=false){
 
 function storeOptions(value){
     //console.log('Storing object');
+    
     chrome.storage.sync.set({"options": value})
         .then(()=>{
             //console.log('Object of PopUp Options has been stored with value');
             //console.log(value);
         })
 }
-   
