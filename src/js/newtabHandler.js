404e282639ad0facba2cdd1450ed1327dea61f27
JeddSJr <55459056+JeddSJr@users.noreply.github.com>
2025-01-17 12:16:19 -0500

    make image caption pop out when image is set as background + small structure changes in newtab page

--- src/js/newtabHandler.js
+++ src/js/newtabHandler.js
@@ -5,8 +5,8 @@ var ADDINFOLISTELEMENTS = document.createElement("ul")
 ADDINFOLISTELEMENTS.className = "list-group "
 var orientation = "landscape";
 
-window.addEventListener('load', function() {
-    
+window.addEventListener('load', function () {
+
     var zoomedImgContainer = document.getElementById("zoomedImgContainer");
     var newTabContainer = document.getElementById("newTabContainer");
 
@@ -19,31 +19,32 @@ window.addEventListener('load', function() {
 
     function changeImageZoom() {
         var zoomedImgHidden = zoomedImgContainer.getAttribute("hidden");
+
         if (zoomedImgHidden) {
             zoomedImgContainer.removeAttribute("hidden");
-            newTabContainer.setAttribute("hidden","hidden");
+            newTabContainer.setAttribute("hidden", "hidden");
             document.body.style.backgroundColor = "black";
         } else {
-            zoomedImgContainer.setAttribute("hidden","hidden");
+            zoomedImgContainer.setAttribute("hidden", "hidden");
             newTabContainer.removeAttribute("hidden");
             document.body.removeAttribute("style");
         }
     }
 
-    document.addEventListener('invalid', 
-        (()=>{
-            return function(e) {e.preventDefault();};
-            })
-        (), 
+    document.addEventListener('invalid',
+        (() => {
+            return function (e) { e.preventDefault(); };
+        })
+            (),
         true
     );
 
-    if(dezoomedImgContainerL || dezoomedImgContainerP){
-        dezoomedImgContainerL.addEventListener("click",changeImageZoom)
-        dezoomedImgContainerP.addEventListener("click",changeImageZoom)
+    if (dezoomedImgContainerL || dezoomedImgContainerP) {
+        dezoomedImgContainerL.addEventListener("click", changeImageZoom)
+        dezoomedImgContainerP.addEventListener("click", changeImageZoom)
     }
 
-    if(zoomedImgContainer){
+    if (zoomedImgContainer) {
         zoomedImgContainer.addEventListener(
             "click",
             changeImageZoom
@@ -52,45 +53,45 @@ window.addEventListener('load', function() {
 
     setUpTopSites()
 
-    dezoomedImgContainer.addEventListener("mouseover",()=>{
+    dezoomedImgContainer.addEventListener("mouseover", () => {
         switchInfoToImg("info")
     })
 
-    additionalInfoDisplay.addEventListener("mouseleave",()=>{
+    additionalInfoDisplay.addEventListener("mouseleave", () => {
         switchInfoToImg("img")
     })
-    
-    additionalInfoDisplay.addEventListener("dblclick",()=>{
+
+    additionalInfoDisplay.addEventListener("dblclick", () => {
         changeImageZoom()
     })
 })
 
 export function setMainImg(museumImage) {
-    
+
     try {
         console.log(museumImage);
-        
+
         var displayedImgL = document.getElementById("displayImgL");
         var displayedImgP = document.getElementById("displayImgP");
         var imgCaption = document.getElementById("imgCaption")
 
         imgCaption.innerHTML = " "
-        
-        displayedImgL.onload = ()=>{
+
+        displayedImgL.onload = () => {
             checKOrientation(displayedImgL)
             displayLoadingState(false)
         }
-        displayedImgP.onload = ()=>{
+        displayedImgP.onload = () => {
             checKOrientation(displayedImgP)
             displayLoadingState(false)
         }
-        
+
         var zoomedImg = document.getElementById("zoomImg")
-        
+
         let displayArtist = museumImage.artist === "" ? "Artist Unknown" : museumImage.artist
 
-        let displayPeriod = museumImage.period === "" ? "Date Unknown" : museumImage.period 
-        var displayTitle = museumImage.title + " - " + displayArtist + " (" + displayPeriod+ ")"
+        let displayPeriod = museumImage.period === "" ? "Date Unknown" : museumImage.period
+        var displayTitle = museumImage.title + " - " + displayArtist + " (" + displayPeriod + ")"
 
         imgCaption.appendChild(document.createTextNode(displayTitle))
 
@@ -99,7 +100,7 @@ export function setMainImg(museumImage) {
 
         displayedImgP.title, displayedImgL.title = displayTitle
         displayedImgP.alt, displayedImgL.alt = museumImage.title;
-         
+
         zoomedImg.src = museumImage.imgSrc
         zoomedImg.title = displayTitle
         zoomedImg.alt = museumImage.title;
@@ -111,30 +112,39 @@ export function setMainImg(museumImage) {
         displayedImgP.dataset.Classification = displayedImgL.dataset.Classification = museumImage.classification;
         //displayedImgP.dataset.Measurements = displayedImgL.dataset.Measurements = museumImage.measurements;
         displayedImgP.dataset.Department = displayedImgL.dataset.Department = museumImage.department;
-        displayedImgP.dataset.Credits = displayedImgL.dataset.Credits = museumImage.creditLine;  
+        displayedImgP.dataset.Credits = displayedImgL.dataset.Credits = museumImage.creditLine;
         displayedImgP.dataset.Link = displayedImgL.dataset.Link = museumImage.objectURL;
 
         MUSEUMIMAGEDISPLAYED = museumImage;
-        
+
     } catch (error) {
         console.log(error);
     }
 }
 
-export function setBackgroundImage(canSetAsBackground=false,museumImage=MUSEUMIMAGEDISPLAYED){
-    if(canSetAsBackground && museumImage){
+export function setBackgroundImage(canSetAsBackground = false, museumImage = MUSEUMIMAGEDISPLAYED) {
+    var imgCaption = document.getElementById("imgCaption")
+    if (canSetAsBackground && museumImage) {
         console.log("Setting background image")
         console.log(museumImage)
         var body = document.body
-        body.style.backgroundImage = "url('"+museumImage.imgSrc+"')"
+        body.style.backgroundImage = "url('" + museumImage.imgSrc + "')"
         body.style.backgroundSize = "cover"
         body.style.backgroundRepeat = "no-repeat"
         body.style.backgroundAttachment = "fixed"
         body.style.backgroundPosition = "center"
         body.style.backgroundColor = "black"
+
+        imgCaption.style.backgroundColor = "rgba(0,0,0,0.5)"
+        imgCaption.style.color = "white"
+        imgCaption.style.marginTop = "10px"
+
     }
-    else{
+    else {
         document.body.removeAttribute("style")
+        imgCaption.style.color = "#717171"
+        imgCaption.style.removeProperty("background-color")
+        imgCaption.style.removeProperty("margin-top")
     }
 }
 
@@ -145,27 +155,27 @@ $('.search-bar-form').submit(function(event) {
 */
 
 function checKOrientation(img) {
-    
+
     var dezoomedImgContainerL = document.getElementById("dezoomedImgContainerLandscape")
     var dezoomedImgContainerP = document.getElementById("dezoomedImgContainerPortrait")
-    
+
     var nWidth = img.naturalWidth
     var nHeight = img.naturalHeight
 
-    if(nWidth < nHeight){ 
+    if (nWidth < nHeight) {
         dezoomedImgContainerP.removeAttribute("hidden")
-        dezoomedImgContainerL.setAttribute("hidden","hidden")
+        dezoomedImgContainerL.setAttribute("hidden", "hidden")
         orientation = "portrait"
     }
-    else{ 
+    else {
         dezoomedImgContainerL.removeAttribute("hidden")
-        dezoomedImgContainerP.setAttribute("hidden","hidden")
+        dezoomedImgContainerP.setAttribute("hidden", "hidden")
         orientation = "landscape"
     }
 }
 
-export function displayLoadingState(isLoading=false){
-   
+export function displayLoadingState(isLoading = false) {
+
     var dezoomedImgContainer = document.getElementById("dezoomedImgContainer")
     var spinnerDiv = document.getElementById("loadingSpinnerDiv")
     var dezoomedImgContainerL = document.getElementById("dezoomedImgContainerLandscape")
@@ -173,47 +183,47 @@ export function displayLoadingState(isLoading=false){
     var additionalInfoDisplay = document.getElementById("additionalInfoDisplay")
 
     var imgCaption = document.getElementById("imgCaption")
-    
-    if(isLoading){
+
+    if (isLoading) {
         spinnerDiv.removeAttribute("hidden")
-        dezoomedImgContainer.setAttribute("hidden","hidden")
-        dezoomedImgContainerL.setAttribute("hidden","hidden")
-        dezoomedImgContainerP.setAttribute("hidden","hidden")
-        additionalInfoDisplay.setAttribute("hidden","hidden")
+        dezoomedImgContainer.setAttribute("hidden", "hidden")
+        dezoomedImgContainerL.setAttribute("hidden", "hidden")
+        dezoomedImgContainerP.setAttribute("hidden", "hidden")
+        additionalInfoDisplay.setAttribute("hidden", "hidden")
         imgCaption.innerHTML = " "
         return 0;
     }
-    else{
-        spinnerDiv.setAttribute("hidden","hidden")
+    else {
+        spinnerDiv.setAttribute("hidden", "hidden")
         dezoomedImgContainer.removeAttribute("hidden")
         return 1;
     }
 }
 
-export async function setAdditionalInfo(canDisplay=CANDISPLAYINFO,isLoading=ISLOADINGSTATE){
+export async function setAdditionalInfo(canDisplay = CANDISPLAYINFO, isLoading = ISLOADINGSTATE) {
     console.log("Setting additional info")
     CANDISPLAYINFO = canDisplay;
     ISLOADINGSTATE = isLoading;
 
     var additionalInfoDisplay = document.getElementById("additionalInfoDisplay")
     var displayedImgL = document.getElementById("displayImgL");
-    var displayedImgP = document.getElementById("displayImgP"); 
+    var displayedImgP = document.getElementById("displayImgP");
 
     var dezoomedImgContainerL = document.getElementById("dezoomedImgContainerLandscape")
     var dezoomedImgContainerP = document.getElementById("dezoomedImgContainerPortrait")
 
     additionalInfoDisplay.innerHTML = " "
     ADDINFOLISTELEMENTS.innerHTML = " "
-    
-    if(canDisplay && isLoading === false){
+
+    if (canDisplay && isLoading === false) {
         let data = await displayedImgP.dataset
         let paragraphs = domStringMapToListElements(data)
-        paragraphs.forEach((paragraph)=>{
+        paragraphs.forEach((paragraph) => {
             ADDINFOLISTELEMENTS.appendChild(paragraph)
         })
         let themeColor = "white"
         if (window.matchMedia) {
-            if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+            if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
                 themeColor = document.querySelector("meta[name=dark-theme-color]").content
             } else {
                 themeColor = document.querySelector("meta[name=light-theme-color]").content
@@ -222,56 +232,57 @@ export async function setAdditionalInfo(canDisplay=CANDISPLAYINFO,isLoading=ISLO
         displayedImgL.style.backgroundColor = themeColor
         displayedImgP.style.backgroundColor = themeColor
     }
-    else{
+    else {
         displayedImgL.style.backgroundColor = "white"
         displayedImgP.style.backgroundColor = "white"
     }
-    
+
 }
 
-function switchInfoToImg(state){
+function switchInfoToImg(state) {
     console.log("Switching info to img")
-    if(!CANDISPLAYINFO){
-        return}
-    
+    if (!CANDISPLAYINFO) {
+        return
+    }
+
 
     var dezoomedImgContainerL = document.getElementById("dezoomedImgContainerLandscape")
     var dezoomedImgContainerP = document.getElementById("dezoomedImgContainerPortrait")
-    
+
     var displayedImgL = document.getElementById("displayImgL");
-    var displayedImgP = document.getElementById("displayImgP"); 
+    var displayedImgP = document.getElementById("displayImgP");
 
     var additionalInfoDisplay = document.getElementById("additionalInfoDisplay")
 
     additionalInfoDisplay.innerHTML = " "
 
-    dezoomedImgContainerL.setAttribute("hidden","hidden")
-    dezoomedImgContainerP.setAttribute("hidden","hidden")
-    additionalInfoDisplay.setAttribute("hidden","hidden")
+    dezoomedImgContainerL.setAttribute("hidden", "hidden")
+    dezoomedImgContainerP.setAttribute("hidden", "hidden")
+    additionalInfoDisplay.setAttribute("hidden", "hidden")
 
-    if(state === "info"){
+    if (state === "info") {
         additionalInfoDisplay.innerHTML += ADDINFOLISTELEMENTS.outerHTML
         additionalInfoDisplay.removeAttribute("hidden")
     }
-    if(state === "img"){
-        if(orientation === "landscape"){ 
+    if (state === "img") {
+        if (orientation === "landscape") {
             dezoomedImgContainerL.removeAttribute("hidden")
         }
-        else{ 
+        else {
             dezoomedImgContainerP.removeAttribute("hidden")
         }
     }
 }
 
-function domStringMapToListElements(domStringMap){
+function domStringMapToListElements(domStringMap) {
     var paragraphs = []
     for (const [key, value] of Object.entries(domStringMap)) {
         let paragraph = document.createElement("li")
-        paragraph.innerHTML = "<b>"+ key + "</b> : "
-        if(key === "Link"){
-            paragraph.innerHTML += "<a href='"+value+"' target='_blank'>"+value+"</a>"
+        paragraph.innerHTML = "<b>" + key + "</b> : "
+        if (key === "Link") {
+            paragraph.innerHTML += "<a href='" + value + "' target='_blank'>" + value + "</a>"
         }
-        else{paragraph.innerHTML += value }
+        else { paragraph.innerHTML += value }
         paragraph.className = "list-group-item"
         paragraphs.push(paragraph)
     }
@@ -283,25 +294,25 @@ function faviconURL(u) {
     url.searchParams.set("pageUrl", u);
     url.searchParams.set("size", "32");
     return url.toString();
-  }
-  
-function setUpTopSites(){
-    var artInfoDivRight = document.getElementById("artInfoDivRight")
-    
-    chrome.topSites.get((topSites)=>{
+}
+
+function setUpTopSites() {
+    var topSitesSection = document.getElementById("topSitesSection")
+
+    chrome.topSites.get((topSites) => {
         var cardsSection = "<div class='row row-cols-md-5'>"
-        topSites.forEach((site)=>{
+        topSites.forEach((site) => {
             var siteFavicon = faviconURL(site.url)
             cardsSection += '<div class="col mb-0">'
             cardsSection += '<div class="card topSitesCard d-flex justify-content-center ">'
-            cardsSection += '<a href="'+site.url+'" target="_blank" class="stretched-link" title="'+site.title+'"></a>'
+            cardsSection += '<a href="' + site.url + '" target="_blank" class="stretched-link" title="' + site.title + '"></a>'
             //cardsSection += '<i class="bi bi-three-dots-vertical align-self-end start-100" fill="black" title="More options"></i>'
-            cardsSection += '<div class="topSitesCardImgSection align-self-center d-flex justify-content-center align-items-center mt-1"><img src="'+siteFavicon+'"></div>'
-            cardsSection += '<p class="topSitesTitle mt-2 ms-2 me-2" >'+site.title+'</p>'
+            cardsSection += '<div class="topSitesCardImgSection align-self-center d-flex justify-content-center align-items-center mt-1"><img src="' + siteFavicon + '"></div>'
+            cardsSection += '<p class="topSitesTitle mt-2 ms-2 me-2" >' + site.title + '</p>'
             cardsSection += '</div>'
-            cardsSection += '</div>'   
+            cardsSection += '</div>'
         })
         cardsSection += "</div>"
-        artInfoDivRight.innerHTML += cardsSection
+        topSitesSection.innerHTML += cardsSection
     })
 }
